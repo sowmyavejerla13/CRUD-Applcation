2172c7acaddfbb9f6f28db0b6d4baf1cff1d92c8
sowmyavejerla <sowmya@innovateapps.com>
2016-11-18 17:29:29 +0530

    added search fun

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,37 +3,56 @@
 Welcome
 @endsection
 @section('content')
-    <div class="row">
+    <div class="row" >
         <div class="col-lg-12">
-            <div class="pull-left">
+            <div class="col-lg-3">
                 <h4>Crud</h4>
             </div>
+           
             <div class="pull-right">
-                <a href="{{route('create')}}"class="btn btn-primary"  >Create new item</a>
+
+                <a href='{{route("create")}}'class="btn btn-primary"  >Create new item</a>
             </div>
         </div>
          <div class="col-lg-12">
-            <table class="table">
+            <table id="grid-basic"class="table">
                 <thead>
                   <tr>
-                    <th>Id</th>
-                    <th>Title</th>
-                    <th>Description</th>
-                    <th style="width: width="280px";">Action</th>
+                    <th  data-column-id="id" data-type="numeric">Id</th>
+                    <th  data-column-id="title" >Title</th>
+                    <th  data-column-id="description">Description</th>
+                    <th data-column-id="actions" data-formatter="actions" data-sortable="false"style="width:280px;">Action</th>
                   </tr>
                 </thead>
                 <tbody>
                     @foreach($items as $item)
-                  <tr>
+                  <tr >
                     <td>{{$item->id}}</td>
                     <td>{{$item->title}}</td>
                     <td>{{$item->description}}</td>
-                    <td><a href="{{route('showdetails',['id'=>$item->id])}}" class="btn btn-info">show</a><a href="{{route('editdetails',['id'=>$item->id])}}" class="btn btn-primary">Edit</a><a href="{{route('delete',['id'=>$item->id])}}" class="btn btn-danger">Delete</a></td>
+                   <!--  <td><a href="{{route('showdetails',['id'=>$item->id])}}" class="btn btn-info">show</a><a href="{{route('editdetails',['id'=>$item->id])}}" class="btn btn-primary">Edit</a><a href="{{route('delete',['id'=>$item->id])}}" class="btn btn-danger">Delete</a></td> -->
                   </tr>
                   @endforeach
                 </tbody>
              </table>
          </div>
     </div>
-{{ $items->render()}}
+    <script >
+   $( document ).ready(function(){
+           $("#grid-basic").bootgrid({
+             formatters: {
+               "actions": function(column, row)
+               {
+                 return "<a  href =\"{{route('showdetails',['id'=>$item->id])}} \"data-id=\"" + row.id + "\" data-title=\"" + row.title + "\" data-description=\"" + row.description + "\" class=\"btn btn-info\"><span class=\"fa fa-pencil\"></span>show</a> " +
+                 "<a  href =\"{{route('editdetails',['id'=>$item->id])}} \"  data-id=\"" + row.id + "\" data-title=\"" + row.title + "\" data-description=\"" + row.description + "\" class=\"btn btn-primary\"><span class=\"fa fa-remove\"></span>edit</a>"+
+                 "<a   href=\"{{route('delete',['id'=>$item->id])}} \"data-id=\"" + row.id + "\"data-title=\"" + row.title + "\" data-description=\"" + row.description + "\" class=\"btn btn-danger\"><span class=\"fa fa-remove\"></span>delete</a>";
+               }
+             }})
+          
+           });
+    var token = '{{Session::token()}}';
+ 
+    </script>
+<!-- $items->render() for pagination-->
 @endsection
+
