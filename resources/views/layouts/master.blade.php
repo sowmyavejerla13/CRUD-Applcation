9844817caf8c9be34fdab5b808ade9428eb686df
sowmyavejerla <sowmya@innovateapps.com>
2016-11-18 17:26:35 +0530

    added search fun

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,13 @@
         <title>@yield('title')</title>
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     	 <link rel="stylesheet" type="text/css" href="{{URL::to('/css/style.css')}}"> 
+      
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-bootgrid/1.3.1/jquery.bootgrid.min.css">
+
+       <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+       <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-bootgrid/1.3.1/jquery.bootgrid.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </head>
     
     <body>
@@ -25,10 +32,9 @@
 	</nav>
        <div class="container">@yield('content')</div>
 
-       <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
-       <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-       <!-- <script src="{{URL::to('/js/style.js')}}"></script>
-       Latest compiled and minified JavaScript -->
-       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+       <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.0/angular.min.js"></script>
+        <script src="{{URL::to('/js/style.js')}}"></script>
+      <!--Latest compiled and minified JavaScript -->
     </body>
 </html>		
